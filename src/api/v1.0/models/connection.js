5d426df48ba2ed887ad9fe8c59f889412cb876c3
Ilshat Ishdavletov <lonlyd@gmail.com>
2020-07-12 15:23:28 +0500

    Change db port

--- src/api/v1.0/models/connection.js
+++ src/api/v1.0/models/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 let dbUrl = process.env.MONGODB_URI || 'localhost:27017';
-// let dbPort = process.env.DB_PORT || 27017;
+let dbPort = process.env.DB_PORT || 27017;
 let dbCollection = process.env.DB_COLLECTION || 'loftsystem';
 let uri = `mongodb://${dbUrl}:${dbPort}/${dbCollection}`;
 mongoose.Promise = global.Promise;
